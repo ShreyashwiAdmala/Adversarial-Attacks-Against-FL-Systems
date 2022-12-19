175fe8fbfc19697abc2b05915fded9b35525ed1d
Shreyashwi Admala <40790630+ShreyashwiAdmala@users.noreply.github.com>
2022-12-19 14:06:37 -0500

    Add files via upload

---
+++ federated_learning/worker_selection/selection_strategy.py
@@ -0,0 +1,14 @@
+#selection_strategy
+
+from abc import abstractmethod
+
+class SelectionStrategy:
+
+    @abstractmethod
+    def select_round_workers(self, workers, poisoned_workers, kwargs):
+        """
+        :param workers: list(int). All workers available for learning
+        :param poisoned_workers: list(int). All workers that are poisoned
+        :param kwargs: dict
+        """
+        raise NotImplementedError("select_round_workers() not implemented")
